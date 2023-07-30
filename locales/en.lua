d95352867aea8c08a896e6bcd7d5661c078c1f40
cynio <115213364+dominiksmolinski3@users.noreply.github.com>
2023-07-30 06:06:40 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,16 @@
+Locales['en'] = {
+  -- Misc
+  ['morphine'] = 'Morphine',
+  ['gauze'] = 'Gauze',
+  ['vicodin'] = 'Vicodin',
+  ['max_item'] = 'You are already carrying enough.',
+  -- Revive
+  ['revive_help'] = 'revive a player',
+  -- Item
+  ['used_morphine'] = 'You have used morphine',
+  ['used_gauze'] = 'You have used gauze',
+  ['not_enough_morphine'] = 'You do not have morphine.',
+  ['not_enough_gauze'] = 'You do not have gauze.',
+  ['not_enough_vicodin'] = 'You do not have vicodin.',
+  ['healed'] = 'You have been treated.',
+}
--- locales/pl.lua
+++ locales/pl.lua
@@ -1,1 +1,17 @@
-
+Locales['pl'] = {
+  -- Misc
+  ['vicodin'] = 'Vicodin',
+  ['gauze'] = 'Gaza',
+  ['morphine'] = 'Morfina',
+  ['max_item'] = 'już masz na sobie dość.',
+  -- Revive
+  ['revive_help'] = 'ożyw gracza',
+  -- Item
+  ['used_vicodin'] = 'zażyłeś vicodin',
+  ['used_morphine'] = 'zażyłeś morfine',
+  ['used_gauze'] = 'użyłeś gazy',
+  ['not_enough_vicodin'] = 'Nie posiadasz vicodinu.',
+  ['not_enough_gauze'] = 'Nie posiadasz gazy.',
+  ['not_enough_morphine'] = 'Nie posiadasz morfiny.',
+  ['healed'] = 'czujesz sie lepiej',
+}
